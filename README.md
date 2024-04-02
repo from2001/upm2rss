92b4a1dbad3ffdf7572110743c081f7069fbce3d
Masahiro Yamaguchi <from2001@hotmail.com>
2024-04-02 18:28:33 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# upm2rss
-Output RSS feed of Unity package version history
+# UPM to RSS Converter
+
+This PHP script converts Unity Package Manager (UPM) package data from JSON format to an RSS 2.0 feed. It is specifically designed to fetch package data from the Unity package registry and transform it into a standardized RSS feed.
+
+## Requirements
+
+- PHP 7.4 or higher
+- Access to the internet to fetch package data from Unity's package registry
+
+## Installation
+
+No specific installation steps are required. Simply place `upm2rss.php` on your web server where PHP is enabled.
+
+## Usage
+
+To use the script, navigate to the location of `upm2rss.php` on your web server and append the query parameter `id` with the Unity package ID you wish to convert to RSS. For example:
+
+`http://yourserver.com/upm2rss.php?id=com.unity.xr.hands`
+
---
+++ upm2rss.php
@@ -0,0 +1,58 @@
+<?php
+
+// Check if the 'id' GET parameter is set
+if (!isset($_GET['id'])) {
+    die('No ID provided');
+}
+
+// Construct the URL to fetch the JSON data
+$id = $_GET['id'];
+$url = "https://packages.unity.com/$id";
+
+// Use context to get HTTP response header
+$context = stream_context_create(array('http' => array('ignore_errors' => true)));
+$jsonData = file_get_contents($url, false, $context);
+
+// Check for 404 or other error codes in the response header
+if (isset($http_response_header) && strpos($http_response_header[0], "404")) {
+    die('Error: Data not found (404)');
+}
+
+// Continue if we have valid data
+if (!$jsonData) {
+    die('Could not retrieve data');
+}
+
+// Decode the JSON data
+$json = json_decode($jsonData, true);
+if (!$json) {
+    die('Invalid JSON data');
+}
+
+// Construct the RSS feed
+header('Content-Type: application/xml; charset=UTF-8');
+echo "<?xml version='1.0' encoding='UTF-8'?>\n";
+echo "<rss version='2.0'>\n";
+echo "<channel>\n";
+
+// Channel section
+echo "<title>" . htmlspecialchars($json['name']) . "</title>\n";
+echo "<link>https://packages.unity.com/" . htmlspecialchars($json['_id']) . "</link>\n";
+echo "<description>" . htmlspecialchars($json['description']) . "</description>\n";
+
+// Items section
+if (isset($json['versions']) && is_array($json['versions'])) {
+    foreach ($json['versions'] as $version) {
+        echo "<item>\n";
+        echo "<title>" . htmlspecialchars($version['name']) . "@" . htmlspecialchars($version['version']) . "</title>\n";
+        echo "<link>" . htmlspecialchars($version['documentationUrl']) . "</link>\n";
+        echo "<description>" . htmlspecialchars($version['_upm']['changelog']) . "</description>\n";
+        // Assuming 'time' is an array with version timestamps
+        echo "<pubDate>" . htmlspecialchars($json['time'][$version['version']]) . "</pubDate>\n";
+        echo "</item>\n";
+    }
+}
+
+echo "</channel>\n";
+echo "</rss>\n";
+?>
